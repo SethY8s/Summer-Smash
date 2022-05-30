867550e1997cb5a68a05ece00608bdcb592a43d1
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-05-30 17:33:15 -0400

    test for making proper rain drop animation

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const startingPoint = document.querySelector('.')
+
+
+
+
+const scrollOptions = {};
+
+const scrollingDrops = new IntersectionObserver(function (
+  entries,
+  scrollingDrops
+) {
+  entries.forEach((entries) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+        entry.target.add('something');
+
+    }
+  });
+},
+scrollOptions);
+
+
