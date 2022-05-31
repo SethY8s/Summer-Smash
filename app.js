053d15c794200f1da3e00b7ae3993e601e095aef
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-05-31 08:21:38 -0400

    changes back for testing

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-const startingPoint = document.querySelector('.rain-div');
+// const startingPoint = document.querySelector('.band-student');
+// const endingPoint = document.querySelector('.sponsors');
 
-const scrollOptions = {};
+// const scrollOptions = {
+//   root: null,
+//   threshold: 0,
+//   rootMargin: '-350px',
+// };
 
-const scrollingDrops = new IntersectionObserver(function (
-  entries,
-  scrollingDrops
-) {
-  entries.forEach((entries) => {
-    if (!entries.isIntersecting) {
-      return;
-    } else {
-      console.log(entries);
-      // entry.target.add('something');
-    }
-  });
-},
-scrollOptions);
+// const scrollingDrops = new IntersectionObserver(function (
+//   entries,
+//   scrollingDrops
+// ) {
+//   entries.forEach((entries) => {
+//     if (!entries.isIntersecting) {
+//       return;
+//     } else {
+//       console.log(entries);
+//       const animationDiv = document.querySelector('.rain-div');
+//       animationDiv.classList.add('rain-div-animation');
+//     }
+//   });
+// },
+// scrollOptions);
 
-scrollingDrops.observe(startingPoint);
+// const dropsEnd = new IntersectionObserver(function (entries, dropsEnd) {
+//   entries.forEach((entries) => {
+//     if (!entries.isIntersecting) {
+//       return;
+//     } else {
+//       console.log(entries);
+//       const animationDiv = document.querySelector('.rain-div');
+//       animationDiv.classList.remove('rain-div-animation');
+//     }
+//   });
+// }, scrollOptions);
+
+// scrollingDrops.observe(startingPoint);
+// dropsEnd.observe(endingPoint);
