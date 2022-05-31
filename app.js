9c5b66f02444074bd49c53fd817e3809b34064c2
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-05-31 17:06:57 -0400

    fixed animation

--- app.js
+++ app.js
@@ -1,50 +1,33 @@
-// const startingPoint = document.querySelector('.band-student');
 const startingPoint = document.querySelector('.at-Nina-June');
-// const endingPoint = document.querySelector('.scroll-endpoint');
 
 const scrollOptions = {
   root: null,
   threshold: 0.77,
-  rootMargin: "-220px"
+  rootMargin: '-220px',
 };
-
-// const endOptions = {
-//     root: null,
-//     threshold: 0,
-//     rootMargin: "-460px"
-//   };
 
 const scrollingDrops = new IntersectionObserver(function (
   entries,
   scrollingDrops
 ) {
-  entries.forEach((entries) => {
-    if (!entries.isIntersecting) {
-      return;
-    } else {
-      console.log(entries);
-      const animationDiv = document.querySelector('.rain-div');
-      animationDiv.classList.add('rain-div-animation');
+    entries.forEach((entries) => {
+      if (!entries.isIntersecting) {
+        return;
+      } else {
+  console.log(entries);
 
-      const thingy = document.querySelector('.background-rain-div');
-      thingy.classList.add('background-rain-div-after');
-    }
-  });
+  const animationDiv = document.querySelector('.rain-div');
+  animationDiv.classList.add('rain-div-animation');
+
+  const thingy = document.querySelector('.background-rain-div');
+  thingy.classList.add('background-rain-div-after');
+
+  const thingyThing = document.querySelector('.finalBackGround');
+  thingyThing.classList.add('finalBackGroundEnd');
+
+  }
+    });
 },
 scrollOptions);
 
-// const dropsEnd = new IntersectionObserver(function (entries, dropsEnd) {
-//   entries.forEach((entries) => {
-//     if (!entries.isIntersecting) {
-//       return;
-//     } else {
-//       console.log(entries);
-//       const animationDiv = document.querySelector('.rain-div');
-//       animationDiv.classList.remove('rain-div-animation');
-//       animationDiv.classList.add('rain-div-end');
-//     }
-//   });
-// }, endOptions);
-
 scrollingDrops.observe(startingPoint);
-// dropsEnd.observe(endingPoint);
