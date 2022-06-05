0eaa62b7d6e54e8b51782e7aad880a908e746b75
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-06-05 10:56:53 -0400

    formatted everything

--- app.js
+++ app.js
@@ -10,33 +10,33 @@ const scrollingDrops = new IntersectionObserver(function (
   entries,
   scrollingDrops
 ) {
-    entries.forEach((entries) => {
-      if (!entries.isIntersecting) {
-        return;
-      } else {
-  console.log(entries);
+  entries.forEach((entries) => {
+    if (!entries.isIntersecting) {
+      return;
+    } else {
+      console.log(entries);
 
-  const animationDiv = document.querySelector('.rain-div');
-  animationDiv.classList.add('rain-div-animation');
+      const animationDiv = document.querySelector('.rain-div');
+      animationDiv.classList.add('rain-div-animation');
 
-  const transparencyAnimation = document.querySelector('.background-rain-div');
-  transparencyAnimation.classList.add('background-rain-div-after');
+      const transparencyAnimation = document.querySelector(
+        '.background-rain-div'
+      );
+      transparencyAnimation.classList.add('background-rain-div-after');
 
-  const fallingAnimation = document.querySelector('.fallingBackGround');
-  fallingAnimation.classList.add('fallingBackGroundEnd');
-
-  }
-    });
+      const fallingAnimation = document.querySelector('.fallingBackGround');
+      fallingAnimation.classList.add('fallingBackGroundEnd');
+    }
+  });
 },
 scrollOptions);
 
 scrollingDrops.observe(startingPoint);
 
 const onLoad = () => {
-    document.getElementById('click-for-scroll-animation').click()
+  document.getElementById('click-for-scroll-animation').click();
+};
 
-}
-
-if(window.screen.width > 900){
-setTimeout(onLoad, 1750)
+if (window.screen.width > 900) {
+  setTimeout(onLoad, 1750);
 }
